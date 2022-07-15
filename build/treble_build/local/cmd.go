280b30c4a662b1d08af1ddbc07c5174209120fb6
Rob Seymour <rseymour@google.com>
2022-07-15 05:53:48 +0000

    Revert "Add profile times to treble_build"
    This reverts commit a241bc30e48dd20765cbff0b3dd4530e779ead86.
    
    Reason for revert:  renderscript_mac (mac) build 8834243
    
    Bug: 239149132
    
    Change-Id: I00c226cd0a70761763a4af8ea988e2923b3ff41c

--- build/treble_build/local/cmd.go
+++ build/treble_build/local/cmd.go
@@ -21,7 +21,6 @@ import (
 	"errors"
 	"io"
 	"os/exec"
-	"syscall"
 	"time"
 )
 
@@ -67,22 +66,10 @@ func run(ctx context.Context, timeout time.Duration, cmdName string, args []stri
 	return &outputBuf, nil, ""
 }
 
-// run without timeout, kill when application dies
-func runNoTimeout(ctx context.Context, cmdName string, args []string) error {
-	cmd := exec.CommandContext(ctx, cmdName, args[0:]...)
-	// Kill when app dies
-	cmd.SysProcAttr = &syscall.SysProcAttr{Pdeathsig: syscall.SIGKILL}
-	err := cmd.Start()
-	if err != nil {
-		return err
-	}
-	cmd.Wait()
-	return nil
-}
-
 // lineScanner
 //
-// Map output lines to strings, with expected number of lines.
+//  Map output lines to strings, with expected number of
+// lines
 type lineScanner struct {
 	Lines []string
 }
